2a65a5dfa4f899ae9498afa8d39c05d1c0ced0c3
Toromo <tdarling.a@gmail.com>
2023-04-21 02:36:20 -0700

    task 2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main is the entry point
+*Return: 0 if successful
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
