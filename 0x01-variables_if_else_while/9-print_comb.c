219261d7b42d66b818e8265a308546aaf91c5790
Toromo <tdarling.a@gmail.com>
2023-04-21 18:37:35 -0700

    task 9 done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 48; n <= 57; n++)
 	{
-		putchar(n + 48);
-			if (n <= 8)
+		putchar(n);
+			if (n <= 56)
 			putchar(',');
 			putchar(' ');
 
