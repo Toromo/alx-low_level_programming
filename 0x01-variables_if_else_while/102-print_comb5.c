b1efce8a3ec2ec3a4f030f7e9a6d5621cad5cfc3
Toromo <tdarling.a@gmail.com>
2023-04-21 20:59:38 -0700

    task 12 done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+*main - the entry point
+*Return: 0 if successful
+*/
+
+int main(void)
+{
+	int n;
+	int m;
+	int x;
+	int y;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (n < m)
+			{
+				putchar((n / 100) + 48);
+				putchar((n % 100) + 48);
+				putchar(' ');
+				putchar((m / 100) + 48);
+				putchar((m % 100) + 48);
+
+					if (n == 98 && m == 99)
+
+						continue;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
