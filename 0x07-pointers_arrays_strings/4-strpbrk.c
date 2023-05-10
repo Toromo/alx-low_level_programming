0212cd85b237b4706f69f6816546a4c1f4f213a7
Toromo <tdarling.a@gmail.com>
2023-05-10 13:35:09 -0700

    task done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: input
+ * Return: a pointer to the byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+		int k;
+
+		while (*s)
+		{
+			for (k = 0; accept[k]; k++)
+			{
+			if (*s == accept[k])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
